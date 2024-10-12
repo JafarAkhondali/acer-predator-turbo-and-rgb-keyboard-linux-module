f8d2b7bed5b12c77770a55a80db5d1eed91e0170
kris <krisdevmail@gmail.com>
2024-10-12 20:21:28 +0300

    Check and enable kernel missing platform_profile module on fresh installs. Attempts to auto-install make.

--- install.sh
+++ install.sh
@@ -13,8 +13,36 @@ fi
 # Remove previous chr devices if any exists
 rm /dev/acer-gkbbl-0 /dev/acer-gkbbl-static-0 -f
 
+fix_missing_make() {
+    if [[ -f /etc/debian_version ]]; then
+        echo "Detected Debian-based system. Proceeding with installation..."
+        sudo apt install make
+        sudo apt-get install linux-headers-$(uname -r) gcc make
+        
+        make || echo "Failed to install Make dependencies. Please install them manually." && exit 1
+    else
+        echo "Not a supported system, you'll have to resolve Make dependencies yourself."
+    fi
+}
 # compile the kernel module
-make || exit 1
+make || fix_missing_make
+
+# check if the profile headers are exported
+if [[ $(grep platform_profile_register /proc/kallsyms) == "" ]]; then
+    echo "[*] Your kernel doesn't seem to export the required symbols."
+    read -p "[*] Do you want to continue (if you know what you're doing)? (y/n): " -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo "[*] Exiting..."
+        exit 1
+    fi
+    
+    echo ""
+
+    # check if the modinfo platform_profile returns a valid output
+    sudo modinfo platform_profile || echo "[*] Your kernel doesn't seem to have the required module" && exit 1
+    sudo modprobe platform_profile || echo "[*] Found but failed to load the platform_profile module" && exit 1
+
+fi
 
 # remove previous acer_wmi module
 rmmod acer_wmi
@@ -25,6 +53,6 @@ modprobe sparse-keymap
 modprobe video
 
 # install facer module
-insmod src/facer.ko
+insmod src/facer.ko || echo "[*] Failed to install facer module" && (dmesg | tail -n 10) && exit 1
 dmesg | tail -n 10
 echo "[*] Done"
